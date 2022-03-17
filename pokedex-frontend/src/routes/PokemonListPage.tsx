042d35c5a5c7b928b5b9b2d3218354b89c3f953b
Guillaume-FAURE <faureg0717@gmail.com>
2022-03-17 17:16:46 +0100

    feat(input): add input to pokemonList

--- pokedex-frontend/src/routes/PokemonListPage.tsx
+++ pokedex-frontend/src/routes/PokemonListPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import NavPage from "../components/NavPage";
 import { SmallDataPokemon } from "../components/SmallDataPokemon";
 import "./PokemonListPage.css";
@@ -7,6 +8,7 @@ export function PokemonListPage() {
     const [idPage, setIdPage] = useState(1);
     const pokemonlist: Array<unknown> = [];
     const controller = new AbortController();
+    const [idInput, setIdInput] = useState(1);
 
     for (let i = 10 * (idPage - 1) + 1; i <= 10 * idPage; i++) {
         i < 899
@@ -14,6 +16,9 @@ export function PokemonListPage() {
                   <SmallDataPokemon idPokemon={i} controller={controller} />
               )
             : console.log("all pokemon charged");
+    }
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setIdInput(parseInt(e.target.value));
     }
 
     return (
@@ -24,6 +29,27 @@ export function PokemonListPage() {
                 pokemonlist={pokemonlist}
                 setIdPage={setIdPage}
             />
+            <form>
+                <label>
+                    Id :
+                    <input
+                        className="inputId"
+                        type="number"
+                        name="id"
+                        min={1}
+                        max={898}
+                        defaultValue={1}
+                        onChange={handleChange}
+                    />
+                </label>
+            </form>
+            <NavLink
+                className="inputNav"
+                to={`/pokemon/${idInput}`}
+                target="_blank"
+            >
+                <button>Go to Id</button>
+            </NavLink>
             <div className="pokemonList">{pokemonlist}</div>
             <NavPage
                 idPage={idPage}
